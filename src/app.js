7c68a20e1d4ff6fa88820662ee4e707d4396dcfb
jorgluiz <jorgeluizti@hotmail.com>
2024-07-03 15:32:29 -0300

    RTCPeerConnection remove

--- src/app.js
+++ src/app.js
@@ -27,7 +27,6 @@ app.get('/viewer', (req, res) => {
     res.status(200).render('home/viewer.html');
 });
 
-const peerConnections = {};
 const viewers = new Set();
 
 io.on('connection', (socket) => {
@@ -37,29 +36,6 @@ io.on('connection', (socket) => {
         console.log('User is ready:', socket.id);
         viewers.add(socket.id);
         io.emit('updateViewers', Array.from(viewers));
-
-        // Emite uma oferta para todos os visualizadores
-        viewers.forEach(viewerId => {
-            if (viewerId !== socket.id) {
-                socket.emit('offer', peerConnections[viewerId].peerConnection.localDescription, viewerId);
-            }
-        });
-
-        // Armazena o `localStream` do usuário
-        if (!peerConnections[socket.id]) {
-            peerConnections[socket.id] = {
-                peerConnection: new RTCPeerConnection({ iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] })
-            };
-            peerConnections[socket.id].peerConnection.onicecandidate = event => {
-                if (event.candidate) {
-                    socket.emit('candidate', event.candidate);
-                }
-            };
-        }
-
-        if (localStream) {
-            peerConnections[socket.id].peerConnection.addStream(localStream);
-        }
     });
 
     socket.on('readyToView', () => {
@@ -67,48 +43,16 @@ io.on('connection', (socket) => {
         io.emit('updateViewers', Array.from(viewers));
     });
 
-    socket.on('offer', async (offer, id) => {
-        console.log(`Received offer from ${id}`);
-        const peerConnection = new RTCPeerConnection({
-            iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
-        });
-
-        peerConnection.onicecandidate = event => {
-            if (event.candidate) {
-                console.log(`Sending candidate to ${id}`);
-                socket.emit('candidate', event.candidate, id);
-            }
-        };
-
-        peerConnection.ontrack = event => {
-            console.log(`Received track from ${id}`);
-            // Adiciona a stream recebida ao visualizador
-            io.to(id).emit('offer', offer, socket.id);
-        };
-
-        await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
-        const answer = await peerConnection.createAnswer();
-        await peerConnection.setLocalDescription(answer);
-        console.log(`Sending answer to ${id}`);
-        socket.emit('answer', answer, id);
-
-        peerConnections[id] = peerConnection;
+    socket.on('offer', (offer, id) => {
+        socket.broadcast.emit('offer', offer, socket.id);
     });
 
-    socket.on('answer', async (answer, id) => {
-        console.log(`Received answer from ${id}`);
-        const peerConnection = peerConnections[id];
-        if (peerConnection) {
-            await peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
-        }
+    socket.on('answer', (answer, id) => {
+        io.to(id).emit('answer', answer, socket.id);
     });
 
-    socket.on('candidate', async (candidate, id) => {
-        console.log(`Received candidate from ${id}`);
-        const peerConnection = peerConnections[id];
-        if (peerConnection) {
-            await peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
-        }
+    socket.on('candidate', (candidate, id) => {
+        io.to(id).emit('candidate', candidate, socket.id);
     });
 
     socket.on('disconnect', () => {
@@ -116,12 +60,6 @@ io.on('connection', (socket) => {
         viewers.delete(socket.id);
         io.emit('updateViewers', Array.from(viewers));
         socket.broadcast.emit('user-disconnected', socket.id);
-
-        const peerConnection = peerConnections[socket.id];
-        if (peerConnection) {
-            peerConnection.close();
-            delete peerConnections[socket.id];
-        }
     });
 });
 
