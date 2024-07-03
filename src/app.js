1b20100296a679d2ae2f258d3511323b21a1a6d8
jorgluiz <jorgeluizti@hotmail.com>
2024-07-03 01:08:55 -0300

    WebRTC

--- src/app.js
+++ src/app.js
@@ -25,19 +25,24 @@ app.get('/viewer', (req, res) => {
 });
 
 const peerConnections = {};
+const viewers = new Set();  // Adiciona um Set para gerenciar os espectadores
 
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
 
     socket.on('ready', () => {
         console.log('User is ready:', socket.id);
+        viewers.add(socket.id);  // Adiciona o usuário à lista de espectadores
+        io.emit('updateViewers', Array.from(viewers));  // Atualiza a lista de espectadores para todos os clientes
     });
 
     socket.on('readyToView', () => {
         console.log('Viewer connected:', socket.id);
+        io.emit('updateViewers', Array.from(viewers));  // Atualiza a lista de espectadores para todos os clientes
     });
 
-    socket.on('offer', (offer) => {
+    socket.on('offer', (offer, id) => {
+        peerConnections[socket.id] = { peerConnection: new RTCPeerConnection({ iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] }), id };
         socket.broadcast.emit('offer', offer, socket.id);
     });
 
@@ -51,6 +56,9 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         console.log('User disconnected:', socket.id);
+        viewers.delete(socket.id);  // Remove o usuário da lista de espectadores
+        io.emit('updateViewers', Array.from(viewers));  // Atualiza a lista de espectadores para todos os clientes
+        delete peerConnections[socket.id];
         socket.broadcast.emit('disconnect', socket.id);
     });
 });
